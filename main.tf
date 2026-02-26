63c18bb6c91609456a17dce1d550af3e545daa9b
surekha25 <surekhasuri25@gmail.com>
2026-02-26 10:23:52 +0530

    terraform

--- main.tf
+++ main.tf
@@ -8,8 +8,15 @@ module "aws_vpc" {
     project_name = var.project
     environment = var.env
     vpc_tags = var.vpc_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
 }
 
 output "vpc_id" {
     value = module.aws_vpc.vpc_id
 }
+
+output "igw" {
+    value = module.aws_vpc.igw
+}   
